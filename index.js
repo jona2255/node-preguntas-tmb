25818900a976a2b44e400b04ba89aed185a90c3a
Jonatan <Snchez>
2021-03-31 12:38:23 +0200

    Creado .env, implementados los argv

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { program } = require("commander");
+const inquirer = require("inquirer");
+const preguntas = require("./preguntas");
 
+
+program
+  .option("-c, --color <color>", "Color de la Línea")
+  .option("-a, --abrev", "Texto de la línea abreviado");
+
+program.parse(process.argv);
+
+const options = program.opts();
+console.log(options);
+
+inquirer.prompt(preguntas)
+  .then(resp => {
+    // resp es un array con las respuestas
+  });
---
+++ preguntas.js
@@ -0,0 +1,5 @@
+const preguntas = [
+
+];
+
+module.exports = preguntas;
